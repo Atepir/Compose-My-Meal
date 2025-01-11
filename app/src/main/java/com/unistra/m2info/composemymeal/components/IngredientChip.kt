3dfd67eb4d30497313d4a98adab15a781b2d2eb6
Atepir <atepir0@gmail.com>
2025-01-11 14:44:11 +0100

    feature: ingredient preview in detail

--- app/src/main/java/com/unistra/m2info/composemymeal/components/IngredientChip.kt
+++ app/src/main/java/com/unistra/m2info/composemymeal/components/IngredientChip.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import coil.compose.AsyncImage
 
 @Composable
 fun IngredientChip(ingredient: String, measure: String) {
@@ -26,17 +27,28 @@ fun IngredientChip(ingredient: String, measure: String) {
             .padding(4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = ingredient.replaceFirstChar { it.uppercase() },
-            style = MaterialTheme.typography.bodyMedium,
-            fontWeight = FontWeight.Medium,
-            modifier = Modifier.weight(1f),
-        )
+        Row (
+            modifier = Modifier.fillMaxWidth(0.7f).height(24.dp),
+        ) {
+            val imageUrl = "https://www.themealdb.com/images/ingredients/${ingredient}-Small.png"
+            AsyncImage(
+                model = imageUrl,
+                contentDescription = ingredient,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = ingredient.replaceFirstChar { it.uppercase() },
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f),
+            )
+        }
         Spacer(modifier = Modifier.width(16.dp))
         Text(
             text = measure.replaceFirstChar { it.uppercase() },
             style = MaterialTheme.typography.bodyMedium,
-            textAlign = TextAlign.End,
+            textAlign = TextAlign.Start,
         )
     }
 }
